fc340d4abcfebb0598669878cbf8fad505dd49a5
Lucy <lucywoodman1@gmail.com>
2020-08-23 11:26:47 -0400

    Added navbar changes and some main content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
