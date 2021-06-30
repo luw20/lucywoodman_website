e191de1158923a77cf7210d2b562fd8044e529e6
Lucy <lucywoodman1@gmail.com>
2021-06-30 17:11:31 -0400

    Made some major improvements to the hamburger menu. It looks much better

--- src/app/hamburger-menu/hamburger-menu.component.ts
+++ src/app/hamburger-menu/hamburger-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hamburger-menu',
@@ -7,13 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HamburgerMenuComponent implements OnInit {
   
-
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @HostListener('click', ['$event.target'])
   showMenuOnClick(): void{
-  
+   console.log("CLicked button");
+   if(document.getElementById("hamburger-content").offsetWidth == 0){
+    document.getElementById("hamburger-content").style.width = "250px";
+   }
+
+   else{
+    document.getElementById("hamburger-content").style.width = "0px";
+   }
+
   }
 }
